0fc53a9e1166d8288671b269e52a1785e470bdee
norbertlienig <norbertlienig@gmail.com>
2023-05-06 11:52:27 +0200

    documentation

--- utils/runner.py
+++ utils/runner.py
@@ -9,7 +9,7 @@ def init_model(path, n_threads=4, n_ctx=2048, use_mlock=True) -> Llama:
     return model
 
 def run(prompt, model, config: Dict[str, any], max_tokens=128, stop=List[str], name=None) -> str:
-    logging.info(f"Running text generation for prompt: {prompt}")
+    print(f"Running text generation for prompt: {prompt}")
 
     if isinstance(model, Llama):
         output = model(prompt,
@@ -24,7 +24,7 @@ def run(prompt, model, config: Dict[str, any], max_tokens=128, stop=List[str], n
     else:
         tokenizer = AutoTokenizer.from_pretrained('mosaicml/mpt-7b-storywriter', use_fast=True)
         input_ids = tokenizer.encode(prompt, return_tensors="pt")
-        logging.info(f"Input Prompt encoded: {input_ids}")
+        print(f"Input Prompt encoded: {input_ids}")
         
         gen_tokens = model.generate(input_ids,
                                      max_length=input_ids.shape[-1] + max_tokens,
@@ -36,6 +36,6 @@ def run(prompt, model, config: Dict[str, any], max_tokens=128, stop=List[str], n
                                      num_return_sequences=1)
         generated_text = tokenizer.decode(gen_tokens[:, input_ids.shape[-1]:][0], skip_special_tokens=True)
 
-    logging.info(f"Generated Output: {generated_text}")
+    print(f"Generated Output: {generated_text}")
 
     return generated_text
